d515e63831bdec23f4ddd3169725d587d741ecea
Alan Wang <xf6wang@gmail.com>
2018-01-25 22:20:01 +0000

    Add ssn and txn tests for HTTP2

--- tests/gold_tests/continuations/openclose_h2.test.py
+++ tests/gold_tests/continuations/openclose_h2.test.py
@@ -17,61 +17,83 @@
 #  limitations under the License.
 
 import os
-from random import randint
 Test.Summary = '''
-Test transactions and sessions, making sure they open and close in the proper order.
+Test transactions and sessions for http2, making sure they open and close in the proper order.
 '''
-# need Apache Benchmark. For RHEL7, this is httpd-tools
 Test.SkipUnless(
-    Condition.HasProgram(
-        "ab", "apache benchmark (httpd-tools) needs to be installed on system for this test to work")
+    Condition.HasProgram("curl", "Curl needs to be installed on system for this test to work"),
+    Condition.HasCurlFeature('http2')
 )
-
+Test.ContinueOnFail = True
 # Define default ATS
-ts = Test.MakeATSProcess("ts", command="traffic_manager")
-
+ts = Test.MakeATSProcess("ts", select_ports=False, command="traffic_manager")
 server = Test.MakeOriginServer("server")
 
 Test.testName = ""
-request_header = {"headers": "GET / HTTP/1.1\r\n\r\n",
-                  "timestamp": "1469733493.993", "body": ""}
+request_header = {"headers": "GET / HTTP/1.1\r\nHost: www.example.com\r\n\r\n", "timestamp": "1469733493.993", "body": ""}
 # expected response from the origin server
 response_header = {"headers": "HTTP/1.1 200 OK\r\nConnection: close\r\n\r\n",
                    "timestamp": "1469733493.993", "body": ""}
 
-Test.PreparePlugin(os.path.join(Test.Variables.AtsTestToolsDir,
-                                'plugins', 'ssntxnorder_verify.cc'), ts)
-
 # add response to the server dictionary
 server.addResponse("sessionfile.log", request_header, response_header)
+
+ts.addSSLfile("ssl/server.pem")
+ts.addSSLfile("ssl/server.key")
+
+ts.Variables.ssl_port = 4443
+ts.Disk.remap_config.AddLine(
+    'map / http://127.0.0.1:{0}'.format(server.Variables.Port)
+)
+
+ts.Disk.ssl_multicert_config.AddLine(
+    'dest_ip=* ssl_cert_name=server.pem ssl_key_name=server.key'
+)
+
 ts.Disk.records_config.update({
     'proxy.config.diags.debug.enabled': 1,
     'proxy.config.diags.debug.tags': 'ssntxnorder_verify.*',
+    'proxy.config.ssl.server.cert.path': '{0}'.format(ts.Variables.SSLDir),
+    'proxy.config.ssl.server.private_key.path': '{0}'.format(ts.Variables.SSLDir),
     'proxy.config.http.cache.http': 0,  # disable cache to simply the test.
-    'proxy.config.cache.enable_read_while_writer': 0
+    'proxy.config.cache.enable_read_while_writer': 0,
+	
+     # enable ssl port
+    'proxy.config.http.server_ports': '{0} {1}:proto=http2;http:ssl'.format(ts.Variables.port, ts.Variables.ssl_port),
+    'proxy.config.ssl.client.verify.server':  0,
+    'proxy.config.ssl.server.cipher_suite': 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES128-SHA256:ECDHE-RSA-AES256-SHA384:AES128-GCM-SHA256:AES256-GCM-SHA384:ECDHE-RSA-RC4-SHA:ECDHE-RSA-AES128-SHA:ECDHE-RSA-AES256-SHA:RC4-SHA:RC4-MD5:AES128-SHA:AES256-SHA:DES-CBC3-SHA!SRP:!DSS:!PSK:!aNULL:!eNULL:!SSLv2',
+    'proxy.config.http2.max_concurrent_streams_in': 65535
 })
 
-ts.Disk.remap_config.AddLine(
-    'map http://127.0.0.1:{0} http://127.0.0.1:{1}'.format(
-        ts.Variables.port, server.Variables.Port)
-)
+# add plugin to assist with test metrics
+Test.PreparePlugin(os.path.join(Test.Variables.AtsTestToolsDir,
+                                'plugins', 'ssntxnorder_verify.cc'), ts)
 
-numberOfRequests = randint(1000, 1500)
+comparator_command = '''
+if test "`traffic_ctl metric get ssntxnorder_verify.{0}.start | cut -d ' ' -f 2`" -eq "`traffic_ctl metric get ssntxnorder_verify.{0}.close | cut -d ' ' -f 2`" ; then\
+     echo yes;\
+    else \
+    echo no; \
+    fi;
+    '''
 
-# Make a *ton* of calls to the proxy!
+cmdstr = 'curl --http2 -k -vs https://127.0.0.1:{0}'.format(ts.Variables.ssl_port)
+numberOfRequests = 25
+
 tr = Test.AddTestRun()
-tr.Processes.Default.Command = 'ab -n {0} -c 10 http://127.0.0.1:{1}/;sleep 5'.format(
-    numberOfRequests, ts.Variables.port)
+# curl with http2
+tr.Processes.Default.Command = 'sleep 10' # ensure stats updated
 tr.Processes.Default.ReturnCode = 0
 # time delay as proxy.config.http.wait_for_cache could be broken
-tr.Processes.Default.StartBefore(
-    server, ready=When.PortOpen(server.Variables.Port))
-tr.Processes.Default.StartBefore(ts, ready=When.PortOpen(ts.Variables.port))
+tr.Processes.Default.StartBefore(server, ready=When.PortOpen(server.Variables.Port))
+tr.Processes.Default.StartBefore(ts, ready=When.PortOpen(ts.Variables.ssl_port))
+tr.Processes.Default.StartAfter( 
+    *Test.ParallelizeCommand(tr, numberOfRequests, cmdstr)
+    )
 tr.StillRunningAfter = ts
 
 # Watch the records snapshot file.
 records = ts.Disk.File(os.path.join(ts.Variables.RUNTIMEDIR, "records.snap"))
-
 
 # Check our work on traffic_ctl
 # no errors happened,
@@ -84,14 +106,6 @@ tr.Processes.Default.Streams.All = Testers.ContainsExpression(
     'ssntxnorder_verify.err 0', 'incorrect statistic return, or possible error.')
 tr.StillRunningAfter = ts
 tr.StillRunningAfter = server
-
-comparator_command = '''
-if test "`traffic_ctl metric get ssntxnorder_verify.{0}.start | cut -d ' ' -f 2`" -eq "`traffic_ctl metric get ssntxnorder_verify.{0}.close | cut -d ' ' -f 2`" ; then\
-     echo yes;\
-    else \
-    echo no; \
-    fi;
-    '''
 
 # number of sessions/transactions opened and closed are equal
 tr = Test.AddTestRun()
